68068b0c47e02322663b4fd35d5e3e9feec8c2b6
buddhabuddy1234 <kwkoepcke@stu.naperville203.org>
2016-05-22 19:01:35 -0500

    Added a storyline? in the code
    ye.

--- game.py
+++ game.py
@@ -1,7 +1,13 @@
 import pygame,time,random
+from Tkinter import *
 #Kenneth Koepcke, Parth Triviality
 #Final Project
 #Dodge the bullets
+#Things could potentially do:
+##Tkinter menu
+##CPickle scores
+##idk
+#ANYTHING WITH INVINCIBILITY DOES NOT WORK AT ALL
 
 disp_width = 800
 disp_height = 600
@@ -15,52 +21,72 @@ foolsgold = (197, 179, 88)
 colours = (black, red, green, blue)
 spec_colours = (foolsgold, gold)
 personImg = pygame.image.load("woman.bmp")
+heartImg = pygame.image.load("heart.jpg")
 gameDisplay = pygame.display.set_mode((disp_width,disp_height))
+
 
 #rogue functions I don't want to change
 def crash(score):
     text = "Time: "
     text += str(score/60) + "s"
-    message_display(text)
+    crash_message_display(text)
     pygame.quit()
     quit()
-
-def message_display(text):
-    display_text = pygame.font.Font("freesansbold.ttf", 100)
+#Dont ask, I can't tell...
+def crash_message_display(text):
+    display_text = pygame.font.Font("freesansbold.ttf", 50)
     textSurf, textRect = text_object(text, display_text)
     textRect.center = ((disp_width/2),(disp_height/2))
     gameDisplay.blit(textSurf, textRect)
     pygame.display.update()
     time.sleep(3)
 
+def message_display(text):
+    display_text = pygame.font.Font("freesansbold.ttf", 50)
+    textSurf, textRect = text_object(text, display_text)
+    textRect.center = ((disp_width/2),(disp_height/2))
+    gameDisplay.blit(textSurf, textRect)
+
         
 def text_object(text, font):
     textSurface = font.render(text, True, blue)
     return textSurface, textSurface.get_rect()
+#Something I might end up finishing
+class Application(Frame):
+    def __init__(self, master):
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.f_label = Label(self, text = "Super Slime Soccer Settings").grid(row = 0, column = 1, sticky = W)
 
 class Block(object):
     """Block class, creates bullets. Has defaulted aspects"""
-    def __init__(self, object_startx = -500, object_starty = random.randrange(0, disp_height),colour = red, speed = 5):
+    def __init__(self, object_startx = -500, object_starty = random.randrange(0, disp_height),colour = red, speed = 5, hard = False):
         """Takes in Object start x, object star y, object speed"""
         #Could use indexing; could be less lazy
-        self.colour = colour
-        self.speed = speed
-        block_magic = random.randrange(0,100)
-        if block_magic == 50:
-            self.colour = random.choice(spec_colours)
-            if self.colour == gold:
-                self.speed = 9
-            if self.colour == foolsgold:
+        self.hard = hard
+        if self.hard == False:
+            self.colour = colour
+            self.speed = speed
+            block_magic = random.randrange(0,1000)
+            if block_magic == 50:
+                self.colour = random.choice(spec_colours)
+                if self.colour == gold:
+                    self.speed = 9
+                if self.colour == foolsgold:
+                    self.speed = 8
+            if self.colour == red:
+                self.speed = 10
+            if self.colour == blue:
                 self.speed = 8
-    
-        if self.colour == red:
-            self.speed = 10
-        if self.colour == blue:
-            self.speed = 8
-        if self.colour == green:
-            self.speed = 3
-        if self.colour == black:
-            self.speed = 7
+            if self.colour == green:
+                self.speed = 3
+            if self.colour == black:
+                self.speed = 7
+        else:
+            self.colour = red
+            self.speed = 9
         self.x = object_startx
         self.y = object_starty
         self.width = 50
@@ -80,17 +106,19 @@ class Block(object):
     def damage(self, Player, score):
         if self.colour == green:
             Player.health += 1
+            Player.speed = 5
         if Player.invincibility == False:
             if self.colour == red:
                 Player.health -= 30
             if self.colour == blue:
                 Player.health -= 1
                 if Player.speed >= 0.02:
-                    Player.speed -= 0.02
+                    Player.speed -= 0.5
             if self.colour == black:
                 Player.health -= 10
-                Player.speed += 0.05
+                Player.speed += 1
             if self.colour == gold:
+                #Doesn't work lol
                 Player.invincibility = True
                 Player.health += disp_height
             if self.colour == foolsgold:
@@ -119,6 +147,7 @@ class Person(object):
     def createPerson(self):
         gameDisplay.blit(personImg, (self.x,self.y))
     def toggleInvincibility(self):
+        #LOTTA LOGIC, NOT MANY RESULTS
         if self.invincibility == False:
             self.invicibility = True
         else:
@@ -143,10 +172,10 @@ class Person(object):
                     self.y_change = -self.speed
                 if event.key == pygame.K_DOWN:
                     self.y_change = self.speed
-##                if event.key == pygame.K_LEFT:
-##                    self.x_change = -self.speed*0.75
-##                if event.key == pygame.K_RIGHT:
-##                    self.x_change = self.speed*0.75
+                if event.key == pygame.K_LEFT:
+                    self.x_change = -self.speed*0.75
+                if event.key == pygame.K_RIGHT:
+                    self.x_change = self.speed*0.75
 
             if event.type == pygame.KEYUP:
                 print "kup"
@@ -160,6 +189,7 @@ class Person(object):
         if self.x > disp_width or self.x < 15 or self.x > 765:
             self.x_change = 0
         self.y += self.y_change
+
 
         if self.y > disp_width or self.y < 0 or self.y > 550:
             self.y_change = 0
@@ -179,12 +209,13 @@ class Person(object):
             health_color = gold
         else:
             health_color = green
-        pygame.draw.rect(gameDisplay, health_color, [disp_width-10,0, 10, self.health])
+        pygame.draw.rect(gameDisplay, health_color, [disp_width-10,0, 10, self.health/10])
 
 
 class Game(object):
     def __init__(self, disp_width, disp_height):
         #Initialize Window
+        hard = False
         pygame.init()
         self.disp_width = disp_width
         self.disp_height = disp_height
@@ -208,6 +239,25 @@ class Game(object):
             gameDisplay.fill(white)
             if score < 60:
                 Player = Person()
+                message_display("Welcome to the program!")
+            if score > 60 and score < 200:
+                message_display("Now die!")
+##            if score > 1200 and score < 1400:
+##                message_display("You're not dead yet..?")
+##            if score > 1400 and score < 1800:
+##                message_display("RNG must really love you! <3 <3")
+##            if score > 1800 and score < 3000:
+##                gameDisplay.blit(heartImg, (0,0))
+##                message_display("I think... I... Love you...")
+##            if score > 3000 and score < 4000:
+##                message_display("LOL, jk. die. :3")
+##                hard = True
+##            if score > 4000 and score < 4200:
+##                message_display("Cheater")
+##            if score > 4200:
+##                Player.x = disp_height/2
+##                Player.y = disp_width/2
+
             y = Player.return_y()
             Player.health_bar()
             health = Player.return_health()
@@ -222,7 +272,7 @@ class Game(object):
             if score/60 == len(level):
                 level += "dango",
                 for i in range(len(level)):
-                    kris_tup.append(Block(random.randrange(-1000, 0),random.randrange(0, disp_height), random.choice(colours)))
+                    kris_tup.append(Block(random.randrange(-1000, 0),random.randrange(0, disp_height), random.choice(colours), 5,hard))
                     
                     
             for dango in kris_tup:
